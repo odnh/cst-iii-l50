3de8c8797e4909d071cf4b15f639f7522c699e16
Ubuntu <L50@vm0.ypvyukc0kflebiau1hoywtwlqe.px.internal.cloudapp.net>
2019-11-25 12:01:25 +0000

    fix exp0

--- exp0.sh
+++ exp0.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 dir=~/data/exp0
 mkdir $dir
-uname -a > "$dir/uname-${hostname}"
-lshw -c cpu > "$dir/cpu-${hostname}"
-lshw -c memory > "$dir/memory-${hostname}"
-lshw -c disk > "$dir/disk-${hostname}"
-lshw -c network > "$dir/network-${hostname"
+sudo uname -a > "$dir/uname-$(hostname)"
+sudo lshw -c cpu > "$dir/cpu-$(hostname)"
+sudo lshw -c memory > "$dir/memory-$(hostname)"
+sudo lshw -c disk > "$dir/disk-$(hostname)"
+sudo lshw -c network > "$dir/network-$(hostname)"
