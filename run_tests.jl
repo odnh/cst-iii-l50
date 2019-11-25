65f11d063a70da38900f2285ef3ee24267a222da
Oliver Hope <oliverdnhope@gmail.com>
2019-11-25 12:06:27 +0000

    add completiong messages, fix syntax

--- run_tests.jl
+++ run_tests.jl
@@ -7,18 +7,22 @@ addprocs(vms)
 
 # Experiment 0: Record machine details
 for i in 1:5
-  remotecall_wait(() -> run(`bash ~/l50-tests/exp0.sh`), i)
+  remotecall_wait(() -> run(`bash '~'/l50-tests/exp0.sh`), i)
 end
+
+println("Experiment 0 Complete")
 
 # Experiment 1: RTT between all machines
 for i in 1:5
   remotecall_wait(() -> pingall(i, "-c 1000 -f", 1), i)
 end
+println("Experiment 1 Complete")
 
 # Expermient 2: Traceroute between all machines
 for i in 1:5
   remotecall_wait(() -> tracerouteall(i, "", 1), i)
 end
+println("Experiment 2 Complete")
 
 # Experiment 3: iperf between all machines (tcp)
 for i in 1:5
@@ -30,6 +34,7 @@ end
 for i in 1:5
   remotecall_wait(() -> iperfservstop(), i)
 end
+println("Experiment 3 Complete")
 
 # Experiment 4: iperf 1 to 2
 
